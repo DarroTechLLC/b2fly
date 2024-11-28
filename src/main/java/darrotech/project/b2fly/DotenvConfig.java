858c0d65da88678cf79385b26c21f1cd7a73574a
DarroTechLLC <darrotech59@gmail.com>
2024-11-27 21:35:06 -0600

    .env configuration with render

--- src/main/java/darrotech/project/b2fly/DotenvConfig.java
+++ src/main/java/darrotech/project/b2fly/DotenvConfig.java
@@ -1,3 +1,32 @@
+//package darrotech.project.b2fly;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.env.ConfigurableEnvironment;
+//import org.springframework.core.env.MapPropertySource;
+//import org.springframework.core.env.MutablePropertySources;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Configuration
+//public class DotenvConfig {
+//
+//    @Bean
+//    public MapPropertySource loadDotenv(ConfigurableEnvironment environment) {
+//        Dotenv dotenv = Dotenv.load();
+//        Map<String, Object> envMap = new HashMap<>();
+//
+//        dotenv.entries().forEach(entry -> envMap.put(entry.getKey(), entry.getValue()));
+//
+//        MapPropertySource propertySource = new MapPropertySource("dotenv", envMap);
+//        MutablePropertySources propertySources = environment.getPropertySources();
+//        propertySources.addFirst(propertySource);
+//
+//        return propertySource;
+//    }
+//}
 package darrotech.project.b2fly;
 
 import io.github.cdimascio.dotenv.Dotenv;
@@ -15,15 +44,22 @@ public class DotenvConfig {
 
     @Bean
     public MapPropertySource loadDotenv(ConfigurableEnvironment environment) {
-        Dotenv dotenv = Dotenv.load();
-        Map<String, Object> envMap = new HashMap<>();
+        // Convert System environment variables to a Map<String, Object>
+        Map<String, Object> systemEnvMap = new HashMap<>();
+        System.getenv().forEach((key, value) -> systemEnvMap.put(key, value));
 
-        dotenv.entries().forEach(entry -> envMap.put(entry.getKey(), entry.getValue()));
+        // Load environment variables from the Render environment
+        MapPropertySource systemEnvPropertySource = new MapPropertySource("systemEnvironment", systemEnvMap);
 
-        MapPropertySource propertySource = new MapPropertySource("dotenv", envMap);
+        // Conditionally load from .env file if not found in the system environment
+        Dotenv dotenv = Dotenv.configure().ignoreIfMalformed().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> systemEnvMap.putIfAbsent(entry.getKey(), entry.getValue()));
+
+        MapPropertySource propertySource = new MapPropertySource("combinedEnvironment", systemEnvMap);
         MutablePropertySources propertySources = environment.getPropertySources();
         propertySources.addFirst(propertySource);
 
         return propertySource;
     }
 }
+
